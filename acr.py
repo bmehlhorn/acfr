53e27b51bd9e4527cda11aec7b98992687b3c3e4
Börge Mehlhorn <b.mehlhorn@ifw-dresden.de>
2022-02-01 17:18:16 +0100

    IO working

--- acr.py
+++ acr.py
@@ -1,6 +1,9 @@
 import itertools as it
 import io 
 import numpy as np
+import argparse
+Data = np.ndarray
+
 
 def main() -> None:
     '''This function is called when the script is explicitly executed'''
@@ -9,18 +12,25 @@ def main() -> None:
     parser.add_argument("-i", "--inputfile",    help="Input *.dat filename")
     parser.add_argument("-o", "--outputfile",   help="Output filename")
     args = parser.parse_args()
-    run(
+    if run(
+            inputfile  = args.inputfile,
             outputfile = args.outputfile,
-            inputfile  = args.outputfile,
-            )
+            ):
+        parser.print_help()
+    
 
 def run(
         inputfile: str,
         outputfile:str,
         ):
+    if not inputfile:
+        return 1
+    if not outputfile:
+        outputfile = f'{inputfile}.acr.dat'
+        print(f'No outputfile specified. Using {outputfile}.') 
     data = read(inputfile)
     data = correct_angle(data)
-    write(data)
+    write(data, outputfile)
 
 
 def read(
@@ -30,14 +40,22 @@ def read(
     with open(filename, 'r') as file:
         lines = file.read()
     lines = lines.strip().split('\n')
-    lines = it.dropwhile(lambda line: not '[Data]' in line, datlines)
+    lines = it.dropwhile(lambda line: not '[Data]' in line, lines)
     next(lines)                 # [Data]
     columns = next(lines)       # columns header
     columns = columns.split(',')
     indexes, names = list(zip(*[
             (idx, name)
             for idx, name in enumerate(columns)
-            if name in ['Rotation Angle (deg)', 'DC Moment Free Ctr (emu)'] 
+            if name in [
+                'Temperature (K)', 
+                'Magnetic Field (Oe)', 
+                'Rotation Angle (deg)', 
+                'DC Moment Fixed Ctr (emu)',
+                'DC Moment Err Fixed Ctr (emu)',
+                'DC Moment Free Ctr (emu)',
+                'DC Moment Err Free Ctr (emu)',
+                ] 
             ]))
     data = np.genfromtxt(
         io.StringIO('\n'.join(lines)), 
@@ -45,10 +63,26 @@ def read(
         delimiter = ',', 
         deletechars = '',
         replace_space = ' ',
+        #names = columns,
         names = names,
         usecols = indexes,
         encoding = encoding,
         )
     return data
-data = correct_angle(data)
-write(data)
+
+def correct_angle(data: Data) -> Data:
+    return data
+
+
+
+def write(
+        data: np.ndarray,
+        outputfile: str,
+        ):
+    np.savetxt(outputfile, data,
+            delimiter = ',',
+            header = 'ACR Output file\n'+', '.join(data.dtype.names),
+            )
+
+if __name__=='__main__':
+    main()
